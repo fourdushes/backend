7c8a8b8b9996b8c57586f87a87f585a9fdaaa47b
yangjoonhyung <kangyang0617@gmail.com>
2026-05-29 17:35:25 +0900

    전체 오류 수정

--- src/main/java/tohear/hearo/global/mail/MailController.java
+++ src/main/java/tohear/hearo/global/mail/MailController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import tohear.hearo.global.Result;
 import tohear.hearo.global.dto.EmailCheckNumberDto;
 import tohear.hearo.global.dto.EmailRequestDto;
-import tohear.hearo.user.ward.UserService;
 
 @RestController
 @RequestMapping("/api/mail")
@@ -17,17 +16,16 @@ import tohear.hearo.user.ward.UserService;
 public class MailController {
 
     private final MailService mailService;
-    private final UserService userService;
 
     @PostMapping("/send")
-    public Result sendMail(@RequestBody EmailRequestDto request) {
+    public Result<?> sendMail(@RequestBody EmailRequestDto request) {
         mailService.sendMail(request.getEmail());
 
         return new Result<>("200", "인증번호가 발송되었습니다.", null);
     }
 
     @PostMapping("/check")
-    public Result checkMail(@RequestBody EmailCheckNumberDto request) {
+    public Result<?> checkMail(@RequestBody EmailCheckNumberDto request) {
         boolean isCorrect = mailService.checkCode(request.getEmail(), request.getCheckNumber());
         if (isCorrect) {
             return new Result<>("200", "인증번호가 일치합니다.", null);
--- src/main/java/tohear/hearo/user/repository/GuardUserRepository.java
+++
@@ -1,9 +0,0 @@
-package tohear.hearo.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tohear.hearo.user.domain.GuardUser;
-
-public interface GuardUserRepository extends JpaRepository<GuardUser, String> {
-
-}
